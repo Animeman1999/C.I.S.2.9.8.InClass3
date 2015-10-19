fe23c16183e7389ce464175714b9eb6d38e763ee
Jeffrey Martin <jmartin5229@mail.kvcc.edu>
2015-10-19 16:14:24 -0400

    Change a lot

---
+++ app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/Crime.java
@@ -0,0 +1,34 @@
+package edu.kvcc.cis298.inclass3.inclass3;
+
+import java.util.UUID;
+
+/**
+ * Created by jmartin5229 on 10/19/2015.
+ */
+public class Crime {
+
+    //Private variables for our models
+    private UUID mId;
+    private String mTitle;
+
+    //Default constructor
+    public Crime () {
+        //
+        mId = UUID.randomUUID();
+    }
+
+
+    // Getters and setters
+    // only need
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
